c2a1fd861e461e706190ae837ba44a422d4dd67c
Ignacio <iopatich@uade.edu.ar>
2026-05-24 12:06:31 -0300

    feature/adapto componente CategoryPill y se adaptan las categorias para la vista de usuario

--- mobile/components/CategoryPill.jsx
+++ mobile/components/CategoryPill.jsx
@@ -3,19 +3,25 @@ import { View, Text } from 'react-native';
 import { categoryPillStyles as styles } from '../styles/components/CategoryPill.js';
 
 export default function CategoryPill({ category }) {
+  const categoryKey = category || 'comun';
+
   const colors = {
-    Platino: { bg: '#40E0D0', text: '#090909' },
-    Oro: { bg: '#E0BF66', text: '#090909' },
-    Especial: { bg: '#FF6347', text: '#090909' },
-    Plata: { bg: '#C0C0C0', text: '#090909' },
-    Común: { bg: '#555555', text: '#FFFFFF' }
+    platino: { bg: '#40E0D0', text: '#090909' },
+    oro: { bg: '#E0BF66', text: '#090909' },
+    especial: { bg: '#FF6347', text: '#090909' },
+    plata: { bg: '#C0C0C0', text: '#090909' },
+    comun: { bg: '#555555', text: '#FFFFFF' }
   };
 
-  const styleConfig = colors[category] || colors['Común'];
+  const styleConfig = colors[categoryKey] || colors['comun'];
+
+  const labelParaMostrar = categoryKey === 'comun'
+    ? 'Común'
+    : categoryKey.chartAt(0).toUpperCase() + categoryKey.slice(1);
 
   return (
     <View style={[styles.pill, { backgroundColor: styleConfig.bg }]}>
-      <Text style={[styles.text, { color: styleConfig.text }]}>{category}</Text>
+      <Text style={[styles.text, { color: styleConfig.text }]}>{labelParaMostrar}</Text>
     </View>
   );
 }
